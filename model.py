b7e74e245d3baa707ee257827123af6aa8409d08
vagrant <vagrant@vagrant.vm>
2017-02-07 00:46:44 +0000

    Began building tables in model.py

--- model.py
+++ model.py
@@ -16,6 +16,50 @@ class User(db.Model):
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password = db.Column(db.String(32), nullable=False)
+    q1_term = db.Column(db.String(20), nullable=True)
+    q2_term = db.Column(db.String(20), nullable=True)
+    q3_term = db.Column(db.String(20), nullable=True)
+    q4_term = db.Column(db.String(20), nullable=True)
+    q5_term = db.Column(db.String(20), nullable=True)
+
+
+class Trip(db.Model):
+    """Trip information which maps to user_id."""
+
+    trip_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    user_id = db.Column(db.Integer, foreign_key=True, nullable=False)
+    days = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.String(128), nullable=False)
+
+
+class Recommendation(db.Model):
+    """One day worth of recommendations for a trip."""
+
+    rec_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    trip_id = db.Column(db.Integer, foreign_key=True, nullable=False)
+    restaurant_id = db.Column(db.Integer, foreign_key=True, nullable=False)
+    activity_id = db.Column(db.Integer, foreign_key=True, nullable=False)
+    day_num = db.Column(db.Integer, nullable=True)
+
+
+class Activity(db.Model):
+    """Single activity recommendation."""
+
+    activity_id = db.Column(db.Integer, autoincrement=True, primary_key=True)  # Can I use a Yelp id here?
+    name = db.Column(db.String(80), nullable=False)
+    location = db.Column(db.String(256), nullable=False)
+    yelp = db.Column(db.String(256), nullable=False)
+    website = db.Column(db.String(256), nullable=False)
+
+
+class Restaurant(db.Model):
+    """Single restaurant recommendation."""
+
+    restaurant_id = db.Column(db.Integer, autoincrement=True, primary_key=True)  # Can I use a Yelp id here?
+    name = db.Column(db.String(80), nullable=False)
+    location = db.Column(db.String(256), nullable=False)
+    yelp = db.Column(db.String(256), nullable=False)
+    website = db.Column(db.String(256), nullable=False)
 
 
 def connect_to_db(app):
